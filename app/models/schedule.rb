572ef39664383fca3e54c56d3e48c1c9641070c3
Mary Major <mary.major@example.comMai>
2020-10-07 14:15:13 +0000

    スケジュールのパーシャル以外完成

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,8 @@
 class Schedule < ApplicationRecord
+    
+    validates :year, presence: true
+    validates :month, presence: true
+    validates :day, presence: true
+    validates :place, presence: true
+    
 end
