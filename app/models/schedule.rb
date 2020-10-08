89aa1729a9ac4c94fdaf0ff9b98f067602bceb5e
Mary Major <mary.major@example.comMai>
2020-10-08 14:33:14 +0000

    バリデーションの修正

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,8 +1,8 @@
 class Schedule < ApplicationRecord
     
-    validates :year, presence: true, numericality: { only_integer: true, greater_than: 2020 } 
-    validates :month, presence: true, numericality: { only_integer: true, greater_than: 1, less_than: 13 }
-    validates :day, presence: true, numericality: { only_integer: true, greater_than: 1, less_than: 32 }
+    validates :year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 2020 } 
+    validates :month, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12 }
+    validates :day, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
     validates :place, presence: true
     
 end
