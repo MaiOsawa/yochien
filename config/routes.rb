77f4e7a9952aa16be862966949f80512675e9c3c
Mary Major <mary.major@example.comMai>
2020-10-11 12:14:05 +0000

    会員登録の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
  
-    resources :schedules 
+    resources :schedules
+    
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_050659) do
+ActiveRecord::Schema.define(version: 2020_10_11_115542) do
 
   create_table "schedules", force: :cascade do |t|
     t.integer "year"
     t.integer "month"
     t.integer "day"
     t.string "place"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
