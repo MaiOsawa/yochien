0ba25f2e5757a078df62920da96765ef2d39787e
Mary Major <mary.major@example.comMai>
2020-10-13 06:04:04 +0000

    root,new.htmlを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
  
     resources :schedules 
+    
     get "signup", to: "session#new"
     post "signup", to: "session#create"
+    get "signin", to: "session#new"
+    post "signin", to: "session#create"
 
 end
