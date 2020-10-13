db237428bce5c6416d5f136bc958e6dfb14ab508
Mary Major <mary.major@example.comMai>
2020-10-13 07:31:24 +0000

    エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
  
     resources :schedules
     
-    get "signup", to: "session#new"
-    post "signup", to: "session#create"
+    get "signup", to: "users#new"
+    post "signup", to: "users#create"
     get "signin", to: "session#new"
     post "signin", to: "session#create"
 
