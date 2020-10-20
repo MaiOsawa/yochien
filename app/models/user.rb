c4ecc5754e9d3e862471ce9bcafa6a829944e67c
Mai <66126460+MaiOsawa@users.noreply.github.com>
2020-10-20 23:15:14 +0900

    Revert "User"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
-    
     has_secure_password
     
     has_many :schedules
     
     validates :name, presence: true, length: { maximum: 20 }
-    validates :email, presence: true, uniqueness: true, length: { maximum: 50 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 40 }
 end
