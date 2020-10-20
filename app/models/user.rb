d1b3c7904bd48c9f3219a2d1c74086f223b8527e
Mary Major <mary.major@example.comMai>
2020-10-20 13:15:58 +0000

    アドレスのバリデーション編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
+    
     has_secure_password
     
     validates :name, presence: true, length: { maximum: 20 }
-    validates :email, presence: true, uniqueness: true, length: { maximum: 40 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 50 }
 end
