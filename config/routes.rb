bfa97162957c75b40a04e2f37983b15dfe06c651
Mary Major <mary.major@example.comMai>
2020-10-22 14:20:28 +0000

    ログアウトボタンのエラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+    
+    root to: "schedules#index"
  
     resources :schedules 
     
     get "signup", to: "users#new"
     post "signup", to: "users#create"
-    get "users/edit", to: "users#edit", as: "edit_user"
+    get "/users/edit", to: "users#edit", as: "edit_user"
     patch "/users", to: "users#update", as: "user"
     get "signin", to: "session#new"
     post "signin", to: "session#create"
