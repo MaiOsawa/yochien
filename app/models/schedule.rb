844e60c9e0960ea148326ebd60b3de102633e077
MaiOsawa <mai_osw@yahoo.co.jp>
2020-11-29 14:49:14 +0000

    dateカラム追加

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,7 @@
 class Schedule < ApplicationRecord
     
     belongs_to :user
-    
+
     validates :year, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 2020 } 
     validates :month, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 12 }
     validates :day, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 31 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_150932) do
+ActiveRecord::Schema.define(version: 2020_11_29_144509) do
 
   create_table "facilities", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_150932) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.datetime "date"
   end
 
   create_table "users", force: :cascade do |t|
