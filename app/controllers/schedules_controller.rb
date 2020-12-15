359d08f37b13252b56dbcd18d67a3adb552fb280
MaiOsawa <mai_osw@yahoo.co.jp>
2020-12-15 14:14:50 +0000

    スケジュールの表示を調整

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -16,7 +16,7 @@ class SchedulesController < UserController
     end
     
     def create
-        schedule_params = params.require(:schedule).permit(:year, :month, :day, :place)
+        schedule_params = params.require(:schedule).permit(:date, :place)
         schedule_params[:user_id] = session[:user_id]
         @schedule = Schedule.new(schedule_params)
         @schedule.date = DateTime.parse("#{@schedule.year}/#{@schedule.month}/#{@schedule.day}")
@@ -33,7 +33,7 @@ class SchedulesController < UserController
     end
     
     def update
-        schedule_params = params.require(:schedule).permit(:year, :month, :day, :place)
+        schedule_params = params.require(:schedule).permit(:date, :place)
         if @schedule.update(schedule_params)
             flash[:notice] = "予定を更新しました！"
             redirect_to schedules_path
--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,11 @@ module Yochien
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
+    # デフォルトのロケールを日本（ja）に設定
+    config.i18n.default_locale = :ja end
 end
