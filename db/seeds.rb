d9793d90254e4ed5f7479b8a549d02c02d27ca0e
MaiOsawa <mai_osw@yahoo.co.jp>
2020-12-19 13:23:40 +0000

    ヘロクへのアップデート

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
 Schedule.create(year: 2020, month: 10, day: 29, place: "ふたば幼稚園")
 User.create(name: "tanaka", email: "tanaka@gmail.com", password: "tanaka", password_confirmation: "tanaka")
+User.create(name: "sample", email: "sample@example.com", password: "123", password_confirmation: "123")
