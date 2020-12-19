da8986608f577482e413e1fc0c589b4c47e0a83e
MaiOsawa <mai_osw@yahoo.co.jp>
2020-12-19 12:59:06 +0000

    ろぐいんRSPEC

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,37 +1,50 @@
 require 'rails_helper'
 
 describe UsersController, type: :request do
-    describe "#index" do
-        context "user exist" do
-            let!(:users){create_list(:user, 3)}
+    describe "#new" do
+        it "response success" do
+            get "/signup"
+            expect(response.status).to eq(200)
+        end
+    end    
+
+    describe "#create" do
+        it "create success" do
+            expect{ post "/signup", params: {
+                    user: {name: "aaa", email: "aa@example.com", password: "12345", password_confirmation: "12345"}
+                }
+            }.to change(User, :count).by(1)
+        end
+    end 
+    
+    describe "#edit" do
+        context "no logged in" do
+            let(:user){create(:user)}
             it "response success" do
                 get "/users/edit"
-                expect(response.status).to eq(302)
+                expect(response.status).to eq(302)  #失敗
+            end
+        end
+        context "logged in" do
+            let(:user){create(:user)}
+            before 'UserIDをセッションから取り出せるようにする' do
+              allow_any_instance_of(ActionDispatch::Request)
+                .to receive(:session).and_return(user_id: user.id)
+            end
+            it "response success" do
+                get "/users/edit"
+                expect(response.status).to eq(200) #成功
             end
         end
     end
     
-    describe "#create" do
-        context "create success" do
-            it "response success" do
-            post "/signup", params: { user: {name: "aaa"}}
-            expect(response).to redirect_to schedules_path(User.last.id)
-        end
-            it "create success" do
-                expect{
-                    post "/signup", params: { user: {name: "aaa"}}
-                }.to change(User, :count).by(0)
-            end
-        end    
-    end  
-    
-    describe "#update" do
-        context "update success" do
-            let!(:user){create(:user, name: "before name")}
-            it "response success" do
-                patch "/users/#{user.id}", params: { user: {name: "after name"}}
-                expect(User.last.name).to eq "after name"
-            end
-        end
-    end    
+    # describe "#update" do
+    #     context "update success" do
+    #         let!(:user){create(:user, name: "before name")}
+    #         it "response success" do
+    #             patch "/users/#{user.id}", params: { user: {name: "after name"}}
+    #             expect(User.last.name).to eq "after name"
+    #         end
+    #     end
+    #end
 end
