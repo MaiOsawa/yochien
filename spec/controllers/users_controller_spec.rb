32cd883030682dfb7fc63d1e7b0aab1dabf9504f
MaiOsawa <mai_osw@yahoo.co.jp>
2020-12-19 12:02:50 +0000

    usersコントローラーRSPEC

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,4 +10,28 @@ describe UsersController, type: :request do
             end
         end
     end
+    
+    describe "#create" do
+        context "create success" do
+            it "response success" do
+            post "/signup", params: { user: {name: "aaa"}}
+            expect(response).to redirect_to schedules_path(User.last.id)
+        end
+            it "create success" do
+                expect{
+                    post "/signup", params: { user: {name: "aaa"}}
+                }.to change(User, :count).by(0)
+            end
+        end    
+    end  
+    
+    describe "#update" do
+        context "update success" do
+            let!(:user){create(:user, name: "before name")}
+            it "response success" do
+                patch "/users/#{user.id}", params: { user: {name: "after name"}}
+                expect(User.last.name).to eq "after name"
+            end
+        end
+    end    
 end
